30b59cf7508b7fb3250f09284c003ee71606d688
Vivek <84849703+vt57299@users.noreply.github.com>
2023-01-15 15:31:45 +0530

    Assignment_1_Question_2

---
+++ Assignment_1_Question_2.py
@@ -0,0 +1,5 @@
+# Write a Python program that accepts a word from the user and reverse it.
+
+word = str(input("Enter any word : "))
+reversed_word = word[::-1]
+print("Reversed word : ", reversed_word)
